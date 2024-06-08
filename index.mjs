348d9be08d5e82d4e93551ded6dbef090bc52621
sanjay <arjunsanjay0@gmail.com>
2024-06-09 02:13:00 +0530

    testing email fetching from gmail api

--- index.mjs
+++ index.mjs
@@ -6,8 +6,10 @@ import { Redis } from "ioredis";
 import RedisStore from "connect-redis";
 
 import passport from "passport";
-
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+import { google } from "googleapis";
+import { OAuth2Client } from "google-auth-library";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -50,6 +52,11 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: [
+        "profile",
+        "email",
+        "https://www.googleapis.com/auth/gmail.readonly",
+      ],
     },
     (accessToken, refreshToken, profile, done) => {
       const user = {
@@ -134,6 +141,60 @@ app.get("/protected", (req, res) => {
     res.send("This is a protected route");
   } else {
     res.redirect("/auth/google");
+  }
+});
+
+const gmail = google.gmail("v1");
+async function fetchOneEmail(accessToken, maxResults = 15) {
+  try {
+    const authClient = new OAuth2Client({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    });
+
+    authClient.setCredentials({
+      access_token: accessToken,
+    });
+
+    const res = await gmail.users.messages.list({
+      auth: authClient,
+      userId: "me",
+      q: "category:primary",
+      maxResults: maxResults,
+    });
+
+    if (!res.data.messages || res.data.messages.length === 0) {
+      throw new Error("No emails found in inbox");
+    }
+
+    const messageId = res.data.messages[14].id;
+
+    const detailRes = await gmail.users.messages.get({
+      auth: authClient,
+      userId: "me",
+      id: messageId,
+    });
+
+    return detailRes;
+  } catch (error) {
+    console.error("Error fetching email:", error);
+    throw error;
+  }
+}
+
+app.get("/get-emails", async (req, res) => {
+  try {
+    if (!req.user || !req.user.accessToken) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const accessToken = req.user.accessToken;
+
+    const emails = await fetchOneEmail(accessToken);
+    res.status(200).json(emails);
+  } catch (error) {
+    console.error("Error fetching emails:", error);
+    res.status(500).json({ error: "An error occurred while fetching emails." });
   }
 });
 
